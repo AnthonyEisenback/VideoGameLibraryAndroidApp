1cc5dd3cfaa894eb184980ca2520808e7ee5205d
anthonyeisenback <anthony.eisenback07@gmail.com>
2018-04-17 13:49:18 -0400

    changed a few colors and strings in the xml files

--- app/src/main/java/com/example/anthonyeisenback/videogamelibraryandroidapp/VideoGameData.java
+++ app/src/main/java/com/example/anthonyeisenback/videogamelibraryandroidapp/VideoGameData.java
@@ -3,7 +3,7 @@ package com.example.anthonyeisenback.videogamelibraryandroidapp;
 import android.app.Application;
 import android.arch.persistence.room.Room;
 
-public class VideoGameData extends Application{
+public class VideoGameData extends Application {
     private VideoGameData database;
 
     public static String DATABASE_NAME = "video_game_database";
@@ -13,7 +13,8 @@ public class VideoGameData extends Application{
         super.onCreate();
 
         Room.databaseBuilder(getApplicationContext(),
-                VideoGameDatabase.class, "database-name").build();
+                VideoGameDatabase.class, DATABASE_NAME).
+                allowMainThreadQueries().build();
     }
 
     public VideoGameData getDatabase() {
